05584b6fa023eef14ef1e1d0ca9d5c698541c07b
Christophe Blonde <cblonde@2A7.42angouleme.fr>
2026-01-20 16:51:14 +0100

    start calculator

--- mobileModule00/ex02/lib/main.dart
+++ mobileModule00/ex02/lib/main.dart
@@ -11,10 +11,46 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        appBar: CustomAppBar(title: 'Calculator'),
+        body: CustomDisplay(),
       ),
     );
+  }
+}
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+
+  const CustomAppBar({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final width = size.width;
+    final height = size.height;
+
+    return AppBar(
+      title: Text(title, style: TextStyle(fontSize: width * 0.03)),
+      backgroundColor: Color(0xFF19bf98),
+      foregroundColor: Color(0xFF1a1470),
+      centerTitle: true,
+      shadowColor: Color(0xFF145243),
+      elevation: height * 0.03,
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class CustomDisplay extends StatelessWidget {
+  const CustomDisplay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final width = size.width;
+    final height = size.height;
+    return Container(width: width * 1, height: height * 0.4);
   }
 }
